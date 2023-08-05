f29dcbf103fe6c4e81d7a18407ec6d1fe97e34d4
Hatim Shakir <hatimshakir9179@gmail.com>
2023-08-06 00:46:11 +0530

    Stage-2 : Added Main Roadmap Code

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -6,34 +6,65 @@
 // local headers
 #include <iostream>
 
-void printBuffer (unsigned char buffer[], int size) {
-  for (int i = 0; i < size; i++) {
-    std::cout << (int) buffer[i] << " ";
-    if (i % 64 == 63) std::cout << "\n";
-  }
-  std::cout << "\n";
+void printBuffer(unsigned char buffer[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << (int)buffer[i] << " ";
+		if (i % 64 == 63)
+			std::cout << "\n";
+	}
+	std::cout << "\n";
 }
 
-int main(int argc, char *argv[]) {
-  Disk disk_run;
+int main(int argc, char *argv[])
+{
+	Disk disk_run;
 
-  // unsigned char buffer[BLOCK_SIZE];
-  // Disk::readBlock(buffer, 7000);
-  // char message[] = "hello";
-  // memcpy(buffer + 20, message, 6);
-  // Disk::writeBlock(buffer, 7000);
+	// create objects for the relation catalog and attribute catalog
+	RecBuffer relCatBuffer(RELCAT_BLOCK);
+	RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  // unsigned char buffer2[BLOCK_SIZE];
-  // char message2[6];
-  // Disk::readBlock(buffer2, 7000);
-  // memcpy(message2, buffer2 + 20, 6);
-  // std::cout << message2;
+	// creating headers for relation catalog and attribute catalog
+	HeadInfo relCatHeader;
+	HeadInfo attrCatHeader;
 
-  unsigned char buffer [BLOCK_SIZE];
-  for (int blockNum = 0; blockNum <= 3; blockNum++) {
-    Disk::readBlock(buffer, blockNum);
-    printBuffer(buffer, BLOCK_SIZE);
-  }
+	// load the headers of both the blocks into relCatHeader and attrCatHeader.
+	// (we will implement these functions later)
+	relCatBuffer.getHeader(&relCatHeader);
+	attrCatBuffer.getHeader(&attrCatHeader);
 
-  return 0;
+	// attrCatBaseSlot stores the base of the slot, 
+	// from which offset should be taken
+	for (int i = 0, attrCatBaseSlot = 0; i < relCatHeader.numEntries; i++)
+	{
+		// will store the record from the relation catalog
+		Attribute relCatRecord[RELCAT_NO_ATTRS]; 
+		relCatBuffer.getRecord(relCatRecord, i);
+
+		printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+		int j = 0;
+		for (; j < relCatHeader.numAttrs; j++)
+		{
+			// declare attrCatRecord and load the attribute catalog entry into it
+			Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+			attrCatBuffer.getRecord(attrCatRecord, attrCatBaseSlot + j);
+
+			// if the current attribute belongs to the current relation
+			// then we print it, which is checked by comparing names
+			if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,
+					   relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
+			{
+				const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER
+										   ? "NUM" : "STR";
+				printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+			}
+		}
+		attrCatBaseSlot += j;
+
+		printf("\n");
+	}
+
+	return 0;
 }
--- references.md
+++ references.md
@@ -1,9 +1,52 @@
-# The Disk Class
+# Basic Terminologies
 
-## `Disk::readBlock`
+### Attributes
+Attribute is just a property, the types a value can have is called a **domain**.
+
+### Records
+Records refer to a collection of **attributes**. This is just a *row* in the database.
+
+### Relation
+Relation is a collection of **records**. Thus, it refers to the *entire table* in the database.
+
+
+
+# Disk Model
+
+## The Disk Class
+
+### `Disk::readBlock`
 - **Args:** `buffer` (denotes the buffer into which data should be brought) and `blockNum` (which disk block should be brought in)
 
-
-## `Disk::writeBlock`
+### `Disk::writeBlock`
 - **Args:** `buffer` (denotes the data which should be written) and `blockNum` (denotes the block to which data should be written)
 
+## Record Block
+1. `REC` : denotes the type of block
+2. `PBlock` : the parent block
+3. `LBlock` : the left block
+4. `RBlock` : the right block
+5. `#Entries` : number of entries in the block
+6. `#Attrs` : number of attributes of each record
+7. `#Slots` : number of slots possible for record entries
+8. `Res` : reserved
+9. `SlotMap` : which slots are empty and which are occupied
+
+## Relation Catalog
+Relation Catalog is used for storing meta-information of the relations in a database. Each entry in the Relation Catalog has a size of 96 bytes and has the following six attributes:
+1. `RelName` 
+2. `#Attributes`
+3. `#Records`
+4. `FirstBlock` 
+5. `LastBlock`
+6. `#Slots` 
+
+## Attribute Catalog
+Attribute Catalog stores meta-information regarding all the attributes of each relation. NITCbase allows Attribute Catalog to be stored over multiple blocks arranged as a linked list.
+
+1. `RelName`
+2. `AttributeName`
+3. `AttributeType`
+4. `PrimaryFlag`
+5. `RootBlock`
+6. `Offset`
