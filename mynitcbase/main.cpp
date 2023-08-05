6a10a7510ca10046b36bd62af65aafe8ec62f387
Hatim Shakir <hatimshakir9179@gmail.com>
2023-08-05 19:00:34 +0530

    Stage-1 : Added Code

---
+++ mynitcbase/main.cpp
@@ -0,0 +1,25 @@
+#include "Buffer/StaticBuffer.h"
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+
+// local headers
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  Disk disk_run;
+
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, 7000);
+  char message[] = "hello";
+  memcpy(buffer + 20, message, 6);
+  Disk::writeBlock(buffer, 7000);
+
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[6];
+  Disk::readBlock(buffer2, 7000);
+  memcpy(message2, buffer2 + 20, 6);
+  std::cout << message2;
+
+  return 0;
+}
---
+++ references.md
@@ -0,0 +1,9 @@
+# The Disk Class
+
+## `Disk::readBlock`
+- **Args:** `buffer` (denotes the buffer into which data should be brought) and `blockNum` (which disk block should be brought in)
+
+
+## `Disk::writeBlock`
+- **Args:** `buffer` (denotes the data which should be written) and `blockNum` (denotes the block to which data should be written)
+
