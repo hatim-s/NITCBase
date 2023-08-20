34337e8cb3d9a9ac316d56ec13d6811209abfd1f
Hatim Shakir <hatimshakir9179@gmail.com>
2023-08-21 00:52:43 +0530

    Stage-5 : Added Roadmap Code

--- mynitcbase/Frontend/Frontend.cpp
+++ mynitcbase/Frontend/Frontend.cpp
@@ -15,13 +15,11 @@ int Frontend::drop_table(char relname[ATTR_SIZE]) {
 }
 
 int Frontend::open_table(char relname[ATTR_SIZE]) {
-  // Schema::openRel
-  return SUCCESS;
+  return Schema::openRel(relname);
 }
 
 int Frontend::close_table(char relname[ATTR_SIZE]) {
-  // Schema::closeRel
-  return SUCCESS;
+  return Schema::closeRel(relname);
 }
 
 int Frontend::alter_table_rename(char relname_from[ATTR_SIZE], char relname_to[ATTR_SIZE]) {
@@ -60,12 +58,6 @@ int Frontend::select_attrlist_from_table(char relname_source[ATTR_SIZE], char re
   // Algebra::project
   return SUCCESS;
 }
-
-// int Frontend::select_from_table_where(char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE],
-//                                       char attribute[ATTR_SIZE], int op, char value[ATTR_SIZE]) {
-//   // Algebra::select
-//   return SUCCESS;
-// }
 
 int Frontend::select_from_table_where(char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE],
                                       char attribute[ATTR_SIZE], int op, char value[ATTR_SIZE]) {
